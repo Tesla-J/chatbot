b42230481a3a5ca0b1b8d5aede354a95e1190810
Tesla-J <rafix995@gmail.com>
2018-08-20 01:00:44 +0100

    Criação do esboço

---
+++ lib/commands.h
@@ -0,0 +1,19 @@
+#define MEM 1024
+
+int
+log(char* input); //Vai registrar a conversa (arquivo criptografado)
+
+int
+interpreter(char+ input); //Vai verificar as Frases e gerar respostas
+
+int
+error(int value); //Vai  gerar erros e reiniciar
+
+int
+data_base(char* frase); //Vai procurar respostas para frases
+
+int
+learn(void); //Aprende uma frase nova
+
+void
+init(void); //inicia todas variaveis globais
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include "lib/commands.h"
+
+void
+main(int argc, char *argv[])
+{
+	char* frase = malloc(1024); //1kb de memoria para os comandos
+
+
+	init();
+}
